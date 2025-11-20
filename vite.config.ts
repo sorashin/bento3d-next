e7353b4d7a89dfbd5ac2c46fb9700d321c1eba44
sorashin <naka52shin@gmail.com>
2025-11-21 04:16:40 +0900

    init時のエラーを消す

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import wasm from 'vite-plugin-wasm'
 import svgr from "vite-plugin-svgr";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     wasm(),
-    // topLevelAwait(),
+    topLevelAwait(),
     svgr({ include: "**/*.svg" })
   ],
   optimizeDeps: {
